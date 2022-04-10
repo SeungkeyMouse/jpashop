85692067096d5ff07cbee4b1bc7d12641da83349
SeungkeyMouse <ksg19980@gmail.com>
2022-04-10 12:38:45 +0900

    테스트: OrderServiceTest, ItemService +) !-OrderRepository.java : EntityManager final변수 선언. 꼭 final이 있어야함.-! +) 주문기능 테스트

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -9,7 +9,7 @@ import javax.persistence.EntityManager;
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Order order){
         em.persist(order);
